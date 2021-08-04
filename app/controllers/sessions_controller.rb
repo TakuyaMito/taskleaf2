4fcf97a82bd81ec0dffd828989cce01c577e285a
Takuya <dyhpaax@gmail.com>
2021-08-04 18:30:42 +0900

    User,Taskの関連付け

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,12 @@
 class SessionsController < ApplicationController
   skip_before_action :login_required
-  
+
   def new
   end
 
   def create
-    user = User.find(email: session_params[:email])
+    user = User.find_by(email: session_params[:email])
+
     if user&.authenticate(session_params[:password])
       session[:user_id] = user.id
       redirect_to root_path, notice: 'ログインしました'
@@ -16,7 +17,7 @@ class SessionsController < ApplicationController
 
   def destroy
     reset_session
-    redirect_to root_path, notice: "ログアウトしました"
+    redirect_to root_path, notice: 'ログアウトしました'
   end
 
   private
